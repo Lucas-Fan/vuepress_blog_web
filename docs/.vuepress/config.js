1fd60226922c8fab46112d66d8ced5f94bbfc2ad
Lucas-Fan <2209253706@qq.com>
2022-03-12 19:11:50 +0800

    feat: add png

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,14 @@ module.exports = {
   title: 'FZYT',
   description: 'A fantastic blog',
   head: [
-    ['link', { rel: 'icon', href: '/image/logo.png' }],
+    ['link', { rel: 'icon', href: '/image/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/image/logo.png' }],
-    ['link', { rel: 'mask-icon', href: '/image/logo.png', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/image/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
